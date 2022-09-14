c3cddba6f968c86c2936c17c416fff7a3f0e3a39
MaxwellChrist <ribthulu@gmail.com>
2022-09-14 15:03:08 -0400

    added js event listener to rating buttons and stored values for thank you state

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 let rating = null
-
-// finder.addEventListener("click", (event) => {
-//     if (rating !== event.target.value) {
-//         rating = event.target.value
-//         console.log(rating)
-//     }
-// })
-
-function findRating() {
-    let finder = document.querySelectorAll(".rating-state-btn-num")
-    finder = Array.from(finder)
-    console.log(finder)
-
-    for (let i = 0; i < finder.length; i++) {
-        console.log(finder[i].value)
+let ratingElement;
+let ratingLength = document.querySelectorAll(".rating-state-btn-num ").length
+let form = document.querySelector(".rating-state-form")
+form.addEventListener("click", findRating, false)
+function findRating(e) {
+    if (e.target !== e.currentTarget || e.target.length <= 1) {
+        if (ratingElement && ratingElement.type !== "submit") {
+            ratingElement.style.backgroundColor = "hsl(212, 30%, 22%)"
+        }
+        e.target.style.backgroundColor = "hsl(25, 97%, 53%)"
+        let clickedItem = e.target.value;
+        rating = clickedItem
+        ratingElement = e.target
+        e.stopPropagation()
     }
-
+    console.log(rating)
 }
-
-// function findRating() {
-//     let finder = document.querySelectorAll(".rating-state-btn-num")
-//     finder = Array.from(finder)
-//     console.log(finder)
-
-//     for (let i = 0; i < finder.length; i++) {
-//         console.log(finder[i].value)
-//     }
-
-// }
-

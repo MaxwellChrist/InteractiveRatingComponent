affc2d6ea1c5e4a9e6f5585efc4e6acf7967c395
MaxwellChrist <ribthulu@gmail.com>
2022-09-15 11:06:46 -0400

    finished submit form function to switch to thank you component; working on styling thank you component

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let rating = null
 let ratingLength = document.querySelectorAll(".rating-state-btn-num ").length
 
-// functions for the rating of the buttons to be stored in variables and css features added accordingly
+// function for the rating of the buttons to be stored in variables and css features added accordingly
 let buttonRatings = document.querySelectorAll(".rating-state-btn-num")
 buttonRatings = Array.from(buttonRatings)
 function fire(e) {
@@ -16,3 +16,20 @@ function fire(e) {
         }
     })
 }
+
+// function for the form to be submitted
+let form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (!rating) {
+        return
+    } else {
+        let ratingDiv = document.querySelector(".rating-state")
+        ratingDiv.setAttribute("id", "currentState")
+        let thankYougDiv = document.querySelector(".thank-you-state")
+        thankYougDiv.removeAttribute("id", "currentState")
+    }
+
+    let span = document.querySelector(".thank-you-state-span")
+    span.innerHTML = rating
+})
